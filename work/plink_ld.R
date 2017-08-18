ef1d9e3b8d78e470fb5bba26847e18156d5bcd6f
Karl Forner <karl.forner@quartz.bio>
2017-08-18 14:39:58 +0200

    added bed_plink_ld_select().
    Former-commit-id: 304d8bf1c730747c2027bef51a6f4177fd2c8e23

--- work/plink_ld.R
+++ work/plink_ld.R
@@ -31,3 +31,22 @@ system.time(ld1 <- bed_plink_ld(bo, window_size = 2, keep_allele_order = FALSE))
 system.time(ld2 <- bed_plink_ld(bo, window_size = 2, keep_allele_order = TRUE))
 #    user  system elapsed
 #  31.308   1.293  30.413
+
+
+system.time(ids1 <- bed_plink_ld_select(bo, threads = 1, window_size = 2, step_size = 1, min_r2 = 0.95))
+#    user  system elapsed
+#  16.140   0.608  16.745
+
+system.time(ids2 <- bed_plink_ld_select(bo, threads = 8, window_size = 2, step_size = 1, min_r2 = 0.95))
+#    user  system elapsed
+#  16.142   0.576  16.716
+identical(ids1, ids2)
+# [1] TRUE
+
+
+system.time(ids2 <- bed_plink_ld_select(bo, keep_allele_order = TRUE, window_size = 2, step_size = 1, min_r2 = 0.95))
+#    user  system elapsed
+#  16.388   0.535  16.927
+identical(ids1, ids2)
+# [1] TRUE
+
