e2570eb01868508c41c532cd55b27bc5aa47d54d
Karl Forner <karl.forner@quartz.bio>
2017-08-31 15:18:39 +0200

    first version of bed_R_lm(), only additive model for now.
    Former-commit-id: a99d4a8c1e6819e0c7ec283fef19803a67575990

--- plinker/R/stats.R
+++ plinker/R/stats.R
@@ -11,40 +11,55 @@
 #' @seealso bed_phenotype_from_df
 bed_R_lm <- function(bo,
   phenotype = bed_fam_df(bo)$PHENO,
-  model = c('additive', 'dominant', 'recessive'))
-{
+  model = c('additive', 'dominant', 'recessive'),
+  covars = NULL
+) {
   nb_snps <- bed_nb_snps(bo)
-
   model <- match.arg(model)
 
-  test <- switch(model,
+  var <- switch(model,
     additive = 'ADD',
     dominant = 'DOM',
     recessive = 'REC', stop('error'))
 
+  if (!is.null(covars)) {
+    check_plink_covars(covars, bed_fam_df(bo))
+  }
+
+  bim <- bed_bim_df(bo)
+  bim <- bim[, c('CHR', 'SNP', 'POS', 'A1')]
+  names(bim) <- c('CHR', 'SNP', 'BP', 'A1')
+
   .process_snp <- function(i) {
     genos <- bed_genotypes(bo, snp_idx = i)
 
-    X <- recode_genotypes(genos, model)
-    df <- data.frame(X = X, Y = phenotype, stringsAsFactors = FALSE)
-    fit <- stats::lm(Y ~ X, data = df, model = FALSE)
+    X <- as.numeric(recode_genotypes(genos, model))
+    df <- data.frame(phenotype, X, stringsAsFactors = FALSE)
+    names(df) <- c('Y', var)
+    fo <- sprintf('Y ~ %s', var)
+    if (!is.null(covars)) {
+      cdf <- covars[, -(1:2), drop = FALSE]
+      fo <- paste0(fo, ' + ', paste0(names(cdf), collapse = ' + '))
+      df <- cbind(df, cdf)
+    }
+
+    fit <- stats::lm(as.formula(fo), data = df, model = TRUE)
     sm <- summary(fit)
 
     data.frame(
-      NMISS = sum(!is.na(X)),
-      BETA = fit$coefficients[2],
-      STAT = sm$coefficients['X', 't value'],
-      P = sm$coefficients['X', 'Pr(>|t|)'],
+      bim[i, , drop = FALSE],
+      TEST = names(df)[-1],
+      NMISS = nrow(fit$model),
+      BETA = fit$coefficients[-1],
+      STAT = sm$coefficients[-1, 't value'],
+      P = sm$coefficients[-1, 'Pr(>|t|)'],
+      row.names = NULL,
       stringsAsFactors = FALSE)
   }
 
   dfs <- lapply(1:nb_snps, .process_snp)
   df <- do.call(rbind, dfs)
 
-  bim <- bed_bim_df(bo)
-  bim <- bim[, c('CHR', 'SNP', 'POS', 'A1')]
-  names(bim) <- c('CHR', 'SNP', 'BP', 'A1')
-
-  cbind(bim, df)
+  df
 }
 
--- plinker/tests/testthat/test-stats.R
+++ plinker/tests/testthat/test-stats.R
@@ -3,11 +3,50 @@ context('R stats')
 .bed_R_lm <- function() {
   bo <- bed_open(plinker:::fetch_sample_bed())
 
-  bo1 <- bed_subset(bo, snp_idx = 10)
+  bo1 <- bed_subset(bo, snp_idx = 10:12)
+  nbind <- bed_nb_samples(bo1)
+
   res <- bed_R_lm(bo1)
+  expect_is(res, 'data.frame')
+  expect_equal(nrow(res), bed_nb_snps(bo1))
+  expect_identical(names(res),
+    c("CHR", "SNP", "BP", "A1", "TEST", "NMISS", "BETA", "STAT", "P"))
 
+  # reproducible
+  expect_identical(bed_R_lm(bo1), res)
 
-  browser()
+  ### phenotype
+  pheno <- 1:nbind
+  bo1 <- bed_subset(bo, snp_idx = 10:12)
+  res2 <- bed_R_lm(bo1, pheno)
+  expect_false(identical(res2, res))
+
+  ### covars
+  # quantitative
+  fam <- bed_fam_df(bo1)
+  covars <- fam[, 1:2]
+
+  # constant covar
+  covars$CONST <- 1
+  res2 <- bed_R_lm(bo1, covars = covars)
+
+  expect_equivalent(res2[res2$TEST == 'ADD', ], res)
+
+  # non constant covars
+  covars <- fam[, 1:2]
+  covars$COVAR1 <- c(rep(1, nbind - 50), c(rep(2, 50)))
+  res2 <- bed_R_lm(bo1, covars = covars)
+  expect_true(abs(res2$P[1] - res$P[1]) > 0.05)
+
+  # categorical covar
+  covars <- fam[, 1:2]
+  covars$CATEG <- c(rep('toto', nbind - 50), c(rep('titi', 50)))
+
+  res2 <- bed_R_lm(bo1, covars = covars)
+  covars$CATEG <- as.factor(covars$CATEG)
+  res3 <- bed_R_lm(bo1, covars = covars)
+  expect_identical(res3, res2)
+
 }
 test_that('bed_R_lm', .bed_R_lm())
 
