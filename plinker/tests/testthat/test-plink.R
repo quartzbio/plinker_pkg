5fc991196c16911b2dc377cc6aa34a3eafc50ad9
Karl Forner <karl.forner@quartz.bio>
2017-09-11 18:02:44 +0200

    moved plink_cmd, transferred some bed_plink_cmd() args to plink_cmd(), added +plink_tfile_to_bfile() and plink_bfile_to_tfile() and save_plink_alleles().
    Former-commit-id: 162f893adb13d8b1753e87b38b1aa98a8c7bb2b0

---
+++ plinker/tests/testthat/test-plink.R
@@ -0,0 +1,92 @@
+context('plink')
+
+.save_plink_alleles <- function() {
+  save_plink_alleles <- plinker:::save_plink_alleles
+  bo <- bed_open(plinker:::fetch_sample_bed())
+
+  bim <- bed_bim_df(bo)
+
+  df <- bim[, c('SNP', 'A1', 'A2')]
+  setup_temp_dir()
+
+  save_plink_alleles(df, 'toto.tall')
+  expect_true(file.exists('toto.tall'))
+
+  df2 <- plinker:::read_plink_output('toto.tall', header = FALSE)
+  names(df2) <- names(df)
+  expect_identical(df2, df)
+}
+test_that('save_plink_alleles', .save_plink_alleles())
+
+
+
+.plink_cmd <- function() {
+  plink_cmd <- plinker:::plink_cmd
+
+  # edge cases
+  suppressWarnings(
+    expect_error(plink_cmd('toto', stderr = TRUE, stdout = TRUE),
+      'ERROR running plink'))
+
+  suppressWarnings(
+    expect_error(plink_cmd('toto', command = '/not/found/foo',
+         stderr = TRUE, stdout = TRUE),
+      'error in running command'))
+
+  text <- plink_cmd('--help', stdout = TRUE)
+  expect_gt(length(text), 100)
+}
+test_that('plink_cmd', .plink_cmd())
+
+
+
+
+.plink_version <- function() {
+  ver <- plink_version()
+  expect_match(ver, "PLINK v")
+}
+test_that('plink_version', .plink_version())
+
+
+
+.plink_bfile_to_tfile_tobfile <- function() {
+  bfile_prefix <- plinker:::fetch_sample_bed()
+  setup_temp_dir()
+  bo1 <- bed_open(bfile_prefix)
+
+  tfile_prefix <- 'plinkt'
+  plink_bfile_to_tfile(bfile_prefix, tfile_prefix, quiet = TRUE)
+  expect_true(all(file.exists(paste0('plinkt.', c('tped', 'tfam')))))
+
+
+  plink_tfile_to_bfile(tfile_prefix, 'toto', quiet = TRUE)
+  expect_true(all(file.exists(paste0('toto.', c('bed', 'fam', 'bim')))))
+
+  # compare them
+  bo1 <- bed_open(bfile_prefix)
+  bo2 <- bed_open('toto')
+
+  g1 <- bed_genotypes_as_strings(bo1)
+  g2 <- bed_genotypes_as_strings(bo2)
+  expect_identical(g1, g2)
+
+  df1 <- bed_bim_df(bo1)
+  df2 <- bed_bim_df(bo2)
+
+  swaps <- which(df1$A1 != df2$A1)
+  expect_equal(swaps, 15)
+  expect_identical(df1$A1[swaps], df2$A2[swaps])
+  expect_identical(df1$A2[swaps], df2$A1[swaps])
+
+  df1_fixed <- df1
+  df1_fixed$A1[swaps] <- df2$A1[swaps]
+  df1_fixed$A2[swaps] <- df2$A2[swaps]
+  expect_identical(df1_fixed, df2)
+
+  g1 <- bed_genotypes(bo1)
+  g2 <- bed_genotypes(bo2)
+
+  g1[, swaps] <- 2L - g1[, swaps]
+  expect_identical(g1, g2)
+}
+test_that('plink_bfile_to_tfile_tobfile', .plink_bfile_to_tfile_tobfile())
