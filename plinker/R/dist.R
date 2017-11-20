603aa9184d373d91ce3f8e46aba5e5a0d48d65e1
Karl Forner <karl.forner@quartz.bio>
2017-11-20 20:07:16 +0100

    improved the interface of the ibs() function, and exported it.

--- plinker/R/dist.R
+++ plinker/R/dist.R
@@ -1,18 +1,26 @@
 #' compute genotypic distances between samples
 #'
-#' @param type 	distance unit type.
+#' @param type 	distance unit type. Currently only ibs
 #' 	cf <https://www.cog-genomics.org/plink/1.9/distance#read_dists>
+#' @inheritSection ibs  missingness
 #' @inheritParams bed_plink_cmd
 #' @return a distance matrix
 #'
 #' @seealso bed_plink_distance
 #' @export
-bed_R_distance <- function(bo, type = c('ibs', 'allele-ct'))
+bed_R_distance <- function(bo, type = c('ibs'))
 {
   type <- match.arg(type)
 
-  mat <- t(bed_genotypes(bo))
-  nb <- bed_nb_samples(bo)
+  dist <- compute_ibs_matrix(bed_genotypes(bo))
+  rownames(dist) <- colnames(dist) <- bed_sample_IDs(bo, custom = TRUE)
+
+  dist
+}
+
+compute_ibs_matrix <- function(genotypes) {
+  mat <- t(genotypes)
+  nb <- ncol(mat)
 
   dist <- matrix(NA, nrow = nb, ncol = nb)
   diag(dist) <- 1
@@ -21,16 +29,30 @@ bed_R_distance <- function(bo, type = c('ibs', 'allele-ct'))
     vi <- mat[, i, drop = TRUE]
     for (j in (i+1):nb) {
       vj <- mat[, j, drop = TRUE]
-      dist[j, i] <- dist[i, j] <- ibs(ibs_state(vi, vj))
+      dist[j, i] <- dist[i, j] <- ibs(vi, vj)
     }
   }
-
-  rownames(dist) <- colnames(dist) <- bed_sample_IDs(bo, custom = TRUE)
 
   dist
 }
 
-ibs <- function(state) {
+
+#' compute the IBS similarity between two vector of genotypes
+#'
+#' Beware: the lengths of genotypes vectors are not checked
+#'
+#' @section missingness:
+#' Currently, it uses what PLINK calls _flat-missing_ missingness correction,
+#' which is the distance when only the non-missing pairs of genotypes
+#' are considered
+#'
+#' @param genos1	first vector of genotypes (NA, 0:2)
+#' @param genos2	second vector of genotypes (NA, 0:2)
+#' @param state		the IBS state vector on which to compute the IBS
+#' @return the IBS distance as a numeric scalar, or NA if no genotype to compare
+#'
+#' @export
+ibs <- function(genos1, genos2, state = ibs_state(genos1, genos2)) {
   nas <- sum(is.na(state))
   nb <- length(state) - nas
   if (nb >= 0)
--- plinker/tests/testthat/test-dist.R
+++ plinker/tests/testthat/test-dist.R
@@ -27,16 +27,22 @@ test_that('ibs_state', .ibs_state())
 .ibs <- function() {
   ibs <- plinker:::ibs
 
-  expect_equal(ibs(0), 0)
-  expect_equal(ibs(1), 0.5)
-  expect_equal(ibs(2), 1)
+  expect_equal(ibs(state = 0), 0)
+  expect_equal(ibs(state = 1), 0.5)
+  expect_equal(ibs(state = 2), 1)
 
-  expect_equal(ibs(rep(2L, 10)), 1)
-  expect_equal(ibs(rep(1L, 10)), 0.5)
-  expect_equal(ibs(rep(0L, 10)), 0)
+  expect_equal(ibs(state = rep(2L, 10)), 1)
+  expect_equal(ibs(state = rep(1L, 10)), 0.5)
+  expect_equal(ibs(state = rep(0L, 10)), 0)
 
-  expect_equal(ibs(c(0,1,2,NA)), 3/6)
-  expect_equal(ibs(NA_integer_), NA_real_)
+  expect_equal(ibs(state = c(0,1,2,NA)), 3/6)
+  expect_equal(ibs(state = NA_integer_), NA_real_)
+
+  expect_equal(ibs(
+    c(0,1,2,NA,2,1,0,1,2,0),
+    c(0,1,2,1 ,0,1,2,0,1,2)),
+    (2*9 - (2 + 2 + 1 + 1 + 2)) / (2*9)
+  )
 
 }
 test_that('ibs', .ibs())
